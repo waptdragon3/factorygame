435bd71017ff1c2fbf996ae9b82f073f4abe534e
waptdragon3 <wthompson786@gmail.com>
2024-10-21 07:41:40 -0500

    added entity behavior framework. very subject to changes

--- src/world/entity.rs
+++ src/world/entity.rs
@@ -1,4 +1,34 @@
+#[derive(Debug, Copy, Clone, Default)]
+struct CraftingBehavior;
+
+
+#[derive(Debug, Copy, Clone, Default)]
+struct BeltBehavior;
+
+
+#[derive(Debug, Copy, Clone, Default)]
+pub enum Behavior {
+    #[default] None,
+    CraftingMachine(CraftingBehavior),
+    TransportBelt(BeltBehavior),
+
+}
+
+#[derive(Debug, Copy, Clone, Default)]
+pub struct Inventory;
+
+
+#[derive(Debug, Copy, Clone, Default)]
+pub struct Blueprint;
+
+
+
+#[derive(Debug, Copy, Clone, Default)]
 pub struct EID (u32);
+#[derive(Debug, Copy, Clone, Default)]
 pub struct Entity {
-    _id: EID
+    _id: EID,
+    pub inventory: Inventory,
+    pub behavior: Behavior,
+    pub bp: Blueprint,
 }
